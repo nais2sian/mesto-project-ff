041e6b5546c5e03f3a4494e4ea6fa07d7b041d40
NasZo <nais2sian@gmail.com>
2024-03-17 21:35:19 +0100

    second commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,27 @@ const pageSection = content.querySelector('.places.page__section');
 const placesList = pageSection.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content; 
 
-function addCard(link, name) {
+function createCard(cardContent, deleteFunc) {
   const cardElement = cardTemplate.querySelector('.places__item.card').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button'); 
 
-  cardElement.querySelector('.card__image').src = link;
-  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = cardContent.link;
+  cardElement.querySelector('.card__title').textContent = cardContent.name;
 
   deleteButton.addEventListener('click', function(){
-    deleteElement(cardElement);
+    deleteFunc(cardElement);
   });
 
-  pageSection.append(cardElement);
+  return (cardElement);
 }
 
 function createCards (){
   initialCards.forEach(function(item){
-    addCard(item.link, item.name); 
+    const cardItem = createCard(item, deleteElement); 
+    pageSection.append(cardItem);
   })
 }
+
 createCards(); 
 
 function deleteElement(targetElement){
